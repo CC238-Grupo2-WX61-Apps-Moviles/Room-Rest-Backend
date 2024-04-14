f1db35be93b77a14ac31d986e7d123e2c48dc149
GabbrM <115942603+GabbrM@users.noreply.github.com>
2024-04-14 14:55:18 -0500

    feat: add entities and dtos

---
+++ src/main/java/com/upc/backend/akira/ecommerce/api/dto/request/ProductRequest.java
@@ -0,0 +1,16 @@
+package com.upc.backend.akira.ecommerce.api.dto.request;
+
+import lombok.Data;
+
+@Data
+public class ProductRequest {
+
+    private Long id;
+    private String nameCategory;
+    private String name;
+    private Double price;
+    private String color;
+    private String category;
+    private String description;
+    private String image;
+}
