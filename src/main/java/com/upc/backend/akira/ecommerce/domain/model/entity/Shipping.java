2d9bf009f7ea210250c03db80a3ebbc48d7d18a4
Nirvana Garcia <84489764+nirvagarcia@users.noreply.github.com>
2024-06-04 19:05:44 -0500

    fixing district in shipping model

--- src/main/java/com/upc/backend/akira/ecommerce/domain/model/entity/Shipping.java
+++ src/main/java/com/upc/backend/akira/ecommerce/domain/model/entity/Shipping.java
@@ -23,7 +23,7 @@ public class Shipping {
     @Column(name = "address", length = 100, nullable = false)
     private String address;
 
-    @Column(name = "district", length = 40, nullable = false)
+    @Column(name = "District", length = 40, nullable = false)
     private String district;
 
     @Column(name = "province", length = 40, nullable = false)
